b6a3f6af84a20ed7302bd581c63d2b944d85a507
Five-sun <tyo1012@naver.com>
2023-05-22 23:20:04 +0900

    Initial commit

---
+++ src/main/java/com/Five_Sun_Seok_Ju/Bugs/Controller/FrontController.java
@@ -0,0 +1,14 @@
+package com.Five_Sun_Seok_Ju.Bugs.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FrontController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "index.html";
+    }
+}
---
+++ src/test/java/com/Five_Sun_Seok_Ju/Bugs/BugsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Five_Sun_Seok_Ju.Bugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
