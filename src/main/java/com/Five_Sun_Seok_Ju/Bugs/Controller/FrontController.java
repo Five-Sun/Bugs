1c0d7e9e8d9b8da5557924d26716a75b23633dc7
Five-sun <tyo1012@naver.com>
2023-05-23 00:00:43 +0900

    test

--- src/main/java/com/Five_Sun_Seok_Ju/Bugs/Controller/FrontController.java
+++ src/main/java/com/Five_Sun_Seok_Ju/Bugs/Controller/FrontController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class FrontController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(){
         return "index.html";
     }
